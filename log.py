2ea9b7680ae733cb42d19ada2a1086f5c3bbdb95
Junaid RASHID <51706577+darjuni123@users.noreply.github.com>
2022-06-06 20:33:27 +0530

    Update log.py

--- log.py
+++ log.py
@@ -6,11 +6,11 @@ def menu():
       while True:
            print("")
            os.system("clear")
-           print('\033[1;36;40m<─────────────── v.1.2 ───────────────>')
+           print('\033[1;36;40m<─────────────── v.2.0 ───────────────>')
            print('')
            os.system('date | lolcat')
            print("\033[1;93m")
-           print(" \033[1;92m  786 => bismi-llāhir-raḥmānir-raḥīm')")
+           print(" \033[1;92m Bismillah hir Rahman Nir Raheem')")
            print("\033[1;93m")
            print("  <───────[ Assalamu-Alaikum ]───────>")
            print("")
